e2ba71c36d0795363c9f44f9ebaef0feca6b22cf
BBOS Software Team <support@bbossystem.com>
2017-12-24 09:10:52 +0700

    upload

---
+++ ReportViewerForMvc.Tests/TestViewDataContainer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReportViewerForMvc.Tests
+{
+    public class TestViewDataContainer : IViewDataContainer
+    {
+        private ViewDataDictionary viewData = new ViewDataDictionary();
+        public ViewDataDictionary ViewData
+        {
+            get
+            {
+                return this.viewData;
+            }
+            set
+            {
+                this.viewData = value;
+            }
+        }
+    }
+}
